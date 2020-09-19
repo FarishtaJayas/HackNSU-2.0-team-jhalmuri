9b0ebdc95f098a7464f4972b8bbb624a35024736
FarishtaJayas <farishtajayas@gmail.com>
2020-09-20 01:09:53 +0600

    REST API works

---
+++ procurebd_api/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from procurebd_api.models import ProfileUser
+
+
+class UserSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = ProfileUser
+        fields = ('name', 'ranking')
+        
+        
+        
